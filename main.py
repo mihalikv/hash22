35f8171c34c6a17ed291977ebd9de2530315f3f5
Viliam Mihálik <viliam.mihalik@smartbase.sk>
2022-02-24 16:57:26 +0100

    triv: init

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import os
+from collections import Counter, OrderedDict
+import numpy as np
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+file_names = ['test']
+input_files = [os.path.join(dir_path, 'input', '{}.txt'.format(file_name)) for file_name in file_names]
+output_files = [os.path.join(dir_path, 'output', '{}.out'.format(file_name)) for file_name in file_names]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def process(input_file_path, output_file_path):
+    output_file = open(output_file_path, 'w')
+
+    with open(input_file_path) as input_file:
+        print(input_file.readlines())
+
+    output_file.write('test')
+    output_file.close()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    for index, input_file_path in enumerate(input_files):
+        process(input_file_path, output_files[index])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    main()
