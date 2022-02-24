d7a7ec1a8fdac248276c1a3d604370cdf49156f7
Viliam Mihálik <viliam.mihalik@smartbase.sk>
2022-02-24 21:20:16 +0100

    triv: third commit

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ file_names = [
     # 'a_an_example.in',
     # 'b_better_start_small.in',
     # 'c_collaboration.in',
-    'd_dense_schedule.in',
+    # 'd_dense_schedule.in',
     # 'e_exceptional_skills.in',
-    # 'f_find_great_mentors.in',
+    'f_find_great_mentors.in',
 ]
 input_files = [os.path.join(dir_path, 'input', '{}.txt'.format(file_name)) for file_name in file_names]
 output_files = [os.path.join(dir_path, 'output', '{}.out'.format(file_name)) for file_name in file_names]
@@ -44,6 +44,7 @@ def process(input_file_path, output_file_path):
         lines = [line.strip() for line in input_file.readlines()]
         num_contrib, num_projects = map(int, lines[0].split(' '))
         contributors = defaultdict(lambda: defaultdict(dict))
+        contributors_by_skill = defaultdict(list)
         projects = {}
         line_index = 0
         for i in range(num_contrib):
@@ -54,6 +55,7 @@ def process(input_file_path, output_file_path):
                 skill, skill_level = lines[line_index].split(' ')
                 contributors[name]['skills'][skill] = int(skill_level)
                 contributors[name]['delay'] = 0
+                contributors_by_skill[skill].append((name, skill_level))
         for i in range(num_projects):
             line_index += 1
             name, num_days, score, deadline, num_roles = lines[line_index].split(' ')
@@ -68,16 +70,19 @@ def process(input_file_path, output_file_path):
                 line_index += 1
                 skill, skill_level = lines[line_index].split(' ')
                 projects[name]['skills_needed'].append((skill, int(skill_level)))
+        for name in contributors_by_skill.keys():
+            contributors_by_skill[name] = list(map(lambda item: item[0], sorted(contributors_by_skill[name], key=lambda x: x[1], reverse=True))) # od najvacsieho
         # print(dict(contributors))
         # print(dict(projects))
     success_projects = {}
 
     is_possible_continue = True
     day_number = 0
+
     while is_possible_continue:
         ids_to_delete = []
-        projects_of_the_day = get_scores_at_day(day_number, projects)
 
+        projects_of_the_day = get_scores_at_day(day_number, projects)
         can_all_do_it = True
         for name, contributor_data in contributors.items():
             is_available = day_number > contributor_data['delay']
@@ -90,19 +95,29 @@ def process(input_file_path, output_file_path):
             project = projects[project_name]
             project_contributors = []
             upgrade_skill_to_contributor = {}
+            project_skills = defaultdict(list)
             for skill_name, skill_level in project['skills_needed']:
-                for name, contributor_data in contributors.items():
+                skill_was_assigned = False
+                for name in contributors_by_skill[skill_name]:
+                # for name, contributor_data in contributors.items():
+                    contributor_data = contributors[name]
+                    is_available = day_number > contributor_data['delay']
+                    if not is_available:
+                        continue
                     skills = contributor_data['skills']
                     has_skill = skill_name in skills and skills[skill_name] >= skill_level
+                    has_mentor = (skill_name in skills and skills[skill_name] == (skill_level - 1)) and skill_level in project_skills[skill]
                     not_in_project = name not in project_contributors
-                    is_available = day_number > contributor_data['delay']
-                    if has_skill and not_in_project and is_available:
+                    if (has_skill or has_mentor) and not_in_project and is_available:
                         # is good for job
+                        skill_was_assigned = True
                         project_contributors.append(name)
-                        if skills[skill_name] == skill_level and skills[skill_name] < 10:
+                        project_skills[skill].append(skill_level)
+                        if (skills[skill_name] == skill_level or skills[skill_name] + 1 == skill_level) and skills[skill_name] < 10:
                             upgrade_skill_to_contributor[name] = skill_name
                         break
-
+                if not skill_was_assigned:
+                    break
             if len(project_contributors) == len(project['skills_needed']):
                 success_projects[project_name] = project_contributors
                 for contributor_name in project_contributors:
@@ -112,16 +127,22 @@ def process(input_file_path, output_file_path):
                 ids_to_delete.append(project_name)
 
         if len(ids_to_delete) == 0 and can_all_do_it:
+            print(f'Tu sme boli {can_all_do_it}')
             is_possible_continue = False
 
         if len(projects_of_the_day) == 0:
+            print(f'Tu sme boli2 {projects_of_the_day}')
             is_possible_continue = False
 
         if len(ids_to_delete) == 0:
             delays = [item['delay'] for item in contributors.values() if item['delay'] >= day_number]
-            min_delay = min(delays) if delays else 1
-            day_number = max(min_delay - day_number, day_number + 1)
+            print(f'delays {delays}')
+            if not delays:
+                is_possible_continue = False
+            min_delay = min(delays)
+            day_number = max(min_delay, day_number + 1)
         else:
+            print(f'ids_to_delete {ids_to_delete}')
             day_number += 1
 
         for id_to_delete in ids_to_delete:
